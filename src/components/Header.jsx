4d42e85a2506ade83149ad28273570af1567ddb9
lucia nambi <nambilucia@gmail.com>
2024-05-26 11:17:56 +0300

    add changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+ function Header() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Category</li>
+        <li>Top rated</li>
+        <li>News</li>
+        <li>Contact us</li>
+
+      </ul>
+    </div>
+  )
+}
+export default Header;
